cd0d511ed2fd5e6a1b2303f8af7ae93a07b6ab24
UME5H-KUMAR <beingtomato7@gmail.com>
2026-05-21 14:25:04 +0530

    Initial commit, added Landing Page with some animated features

--- url-shortener-react/eslint.config.js
+++ url-shortener-react/eslint.config.js
@@ -4,6 +4,15 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
+
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+})
 export default defineConfig([
   globalIgnores(['dist']),
   {
